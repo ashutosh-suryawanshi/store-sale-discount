d7beb85094556fe5e7c4ee668a137bf73ab5f5b5
Ashutosh Suryawanshi <ashutosh.surya007@gmail.com>
2024-04-06 13:27:05 +0530

    Created basic for logic

--- sale_discount.js
+++ sale_discount.js
@@ -1,7 +1,54 @@
-// Input: List of items
+const itemsInventory = [
+  {itemName: "Milk", itemUnitPrice: 3.97, itemSalePrice: {saleQuantity: 2, salePrice: 5.00}},
+  {itemName: "Bread", itemUnitPrice: 2.17, itemSalePrice: {saleQuantity: 3, salePrice: 6.00}},
+  {itemName: "Banana", itemUnitPrice: 0.99},
+  {itemName: "Apple", itemUnitPrice: 0.89}
+]
 
-// Processing: Calculate total price & saved amount based on sale price of an item
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// Output: 
-// Total price: <total_price>
-// You saved <saved_amount> today.
+class Billing {
+  _itemsBought = []
+
+  constructor(itemsBought){
+    this._itemsBought = itemsBought
+  }
+
+  // Calculate total price of all the purchased items
+  calculateTotalPrice(){
+    totalPrice = 0
+    // Calculate total price here
+    return totalPrice
+  }
+
+  // Calculate total discounted price of all the purchased items
+  calculateTotalDiscountedPrice(){
+    totalDiscountedPrice = 0
+    // Calculate total price here
+    return totalDiscountedPrice
+  }
+  
+  // Calculate total saved price
+  calculateSavedPrice(){
+    totalSaved = calculateTotalPrice() - calculateTotalDiscountedPrice()
+    return totalSaved
+  }
+}
+
+// Ask user to enter list of purchased items
+readline.question(`Please enter all the items purchased separated by a comma `, (items) => {
+
+  const itemsBought = items.split(",").map(item => item.trim())
+  const billing = new Billing(itemsBought)
+
+  totalPrice = billing.calculateTotalPrice()
+  console.log("Total price: ", totalPrice)
+
+  totalDiscount = billing.calculateDiscountPrice()
+  console.log("Total discount: ", totalDiscount)
+
+  readline.close();
+});
