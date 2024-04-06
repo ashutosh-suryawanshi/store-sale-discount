906d67ab2ccb718e95cfe1d8eab65766f307d4ea
Ashutosh Suryawanshi <ashutosh.surya007@gmail.com>
2024-04-06 13:40:57 +0530

    applied eslint

--- sale_discount.js
+++ sale_discount.js
@@ -1,54 +1,51 @@
 const itemsInventory = [
-  {itemName: "Milk", itemUnitPrice: 3.97, itemSalePrice: {saleQuantity: 2, salePrice: 5.00}},
-  {itemName: "Bread", itemUnitPrice: 2.17, itemSalePrice: {saleQuantity: 3, salePrice: 6.00}},
-  {itemName: "Banana", itemUnitPrice: 0.99},
-  {itemName: "Apple", itemUnitPrice: 0.89}
-]
+  { itemName: 'Milk', itemUnitPrice: 3.97, itemSalePrice: { saleQuantity: 2, salePrice: 5.00 } },
+  { itemName: 'Bread', itemUnitPrice: 2.17, itemSalePrice: { saleQuantity: 3, salePrice: 6.00 } },
+  { itemName: 'Banana', itemUnitPrice: 0.99 },
+  { itemName: 'Apple', itemUnitPrice: 0.89 },
+];
 
-const readline = require("readline").createInterface({
+const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 class Billing {
-  _itemsBought = []
-
-  constructor(itemsBought){
-    this._itemsBought = itemsBought
+  constructor(itemsBought) {
+    this.itemsBought = itemsBought;
   }
 
   // Calculate total price of all the purchased items
-  calculateTotalPrice(){
-    let totalPrice = 0
+  calculateTotalPrice() {
+    const totalPrice = 0;
     // Calculate total price here
-    return totalPrice
+    return totalPrice;
   }
 
   // Calculate total discounted price of all the purchased items
-  calculateTotalDiscountedPrice(){
-    let totalDiscountedPrice = 0
+  calculateTotalDiscountedPrice() {
+    const totalDiscountedPrice = 0;
     // Calculate total price here
-    return totalDiscountedPrice
+    return totalDiscountedPrice;
   }
-  
+
   // Calculate total saved price
-  calculateSavedPrice(){
-    const totalSaved = this.calculateTotalPrice() - this.calculateTotalDiscountedPrice()
-    return totalSaved
+  calculateSavedPrice() {
+    const totalSaved = this.calculateTotalPrice() - this.calculateTotalDiscountedPrice();
+    return totalSaved;
   }
 }
 
 // Ask user to enter list of purchased items
-readline.question(`Please enter all the items purchased separated by a comma `, (items) => {
+readline.question('Please enter all the items purchased separated by a comma ', (items) => {
+  const itemsBought = items.split(',').map((item) => item.trim());
+  const billing = new Billing(itemsBought);
 
-  const itemsBought = items.split(",").map(item => item.trim())
-  const billing = new Billing(itemsBought)
+  const totalPrice = billing.calculateTotalPrice();
+  console.log('Total price: ', totalPrice);
 
-  const totalPrice = billing.calculateTotalPrice()
-  console.log("Total price: ", totalPrice)
-
-  const totalDiscount = billing.calculateSavedPrice()
-  console.log("Total discount: ", totalDiscount)
+  const totalDiscount = billing.calculateSavedPrice();
+  console.log('Total discount: ', totalDiscount);
 
   readline.close();
 });
