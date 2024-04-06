da98824b1d830c3dedf52c2b1dc051db44cf4121
Ashutosh Suryawanshi <ashutosh.surya007@gmail.com>
2024-04-06 12:20:57 +0530

    Initialized the script

---
+++ sale_discount.js
@@ -0,0 +1,7 @@
+// Input: List of items
+
+// Processing: Calculate total price & saved amount based on sale price of an item
+
+// Output: 
+// Total price: <total_price>
+// You saved <saved_amount> today.
